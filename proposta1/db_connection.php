be4237e741ba20e9fb15430b672c48eb14fa028a
Roger <rsobrino@xtec.cat>
2023-11-09 13:52:47 +0100

    Afegits arxius per l'examen Pe2 de la UF1 del 10 de novembre

---
+++ proposta1/db_connection.php
@@ -0,0 +1,24 @@
+<?php
+    /*
+    En aquest arxiu cal afegir per a que connecti amb la BBDD.
+        - Dades BBDD:
+            * Nom BBDD: users
+            * Nom taula: user
+            * Usuari: root
+            * Contrasenya: 
+            * host: localhost
+        - La connexió pot ser procedimental o objectes (mysqli o PDO). Pero si es barreja, 
+          per molt que funcioni serà errònia.
+        - Verificació de conexió try-catch
+    */
+
+    //Assignació dades BBDD
+    $db_host = "";
+    $db_user = "";
+    $db_passwd = "";
+    $db_name = "";
+
+    //Connexió dintre de try-catch
+        
+
+?>
